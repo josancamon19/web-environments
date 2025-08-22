385e73b7b21bdeade258e3dcb223f452111df56f
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2025-08-22 11:25:48 -0700

    minor storage config dir change

--- src/config/storage_config.py
+++ src/config/storage_config.py
@@ -1,7 +1,7 @@
 import os
 
 ROOT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-DATA_DIR = os.path.join(ROOT_DIR, "data")
+DATA_DIR = os.path.join("data")
 SCREENSHOTS_DIR = os.path.join(DATA_DIR, "screenshots")
 VIDEOS_DIR = os.path.join(DATA_DIR, "videos")
 DB_PATH = os.path.join(DATA_DIR, "tasks.db")
